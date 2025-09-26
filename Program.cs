c5caa7332d83a9ff7a7ef9df36e141779887e38e
Thyago Ladislau <thyagothyago600@gmail.com>
2025-09-26 20:47:43 -0300

    Programa feito

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string fraseUsuario, fraseComDislalia;
+
+Console.Write("Digite uma flase: ");
+fraseUsuario = Console.ReadLine()!;
+
+
+fraseComDislalia = fraseUsuario
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine(fraseComDislalia);
